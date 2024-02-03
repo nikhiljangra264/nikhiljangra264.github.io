7780768688164ae1836e75478b535727b6f674cf
Nikhil <nikhiljangra264@gmail.com>
2024-02-03 10:00:08 +0530

    fixed navbar

---
+++ scrolling_script.js
@@ -0,0 +1,23 @@
+document.querySelectorAll('.navbar-nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+
+        // Close the collapsed navbar
+        const navbarToggler = document.querySelector('.navbar-toggler');
+        const navbarCollapse = document.querySelector('.navbar-collapse');
+
+        if (navbarToggler && navbarCollapse) {
+            navbarToggler.classList.add('collapsed');
+            navbarCollapse.classList.remove('show');
+        }
+    });
+});
